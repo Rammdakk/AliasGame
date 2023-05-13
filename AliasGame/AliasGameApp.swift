34149ac3befc89c2e4c05a9e970b9cf49049e533
Rammdakk <rammdakk@gmail.com>
2023-05-13 12:38:59 +0300

    Initial Commit

---
+++ AliasGame/AliasGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliasGameApp.swift
+//  AliasGame
+//
+//  Created by Рамиль Зиганшин on 13.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AliasGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
