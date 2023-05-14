05978a8e91eaffbecee78f1495d2858b7cf279cd
Marina <maroschupkina@yandex-team.ru>
2023-05-14 23:59:34 +0300

    room screen started

--- AliasGame/Rooms/RoomsScreen.swift
+++ AliasGame/Rooms/RoomsScreen.swift
@@ -7,12 +7,70 @@
 
 import SwiftUI
 
+
+struct RoomModel: Codable {
+    var name: String
+}
+
 struct RoomsScreen: View {
     @Binding var navigationState: NavigationState
+    let roomsMock = [RoomModel(name: "first"),
+                 RoomModel(name: "second"),
+                 RoomModel(name: "third"),
+                 RoomModel(name: "very long room name for absolutely no reason aaaaa aaaaaaaaa fjfjfjfjf sjfsuu sjsjsjs djdjdjd iwiiw djdjdj"),
+                 RoomModel(name: "thirkxkd"),
+                 RoomModel(name: "sksksks"),
+                 RoomModel(name: "sskksks=="),
+                 RoomModel(name: "q9wei"),
+                 RoomModel(name: "ividi"),
+    ]
     
     var body: some View {
-        Text("Rooms")
+        List{
+            ForEach(roomsMock, id: \.name) { item in
+                room(model: item)
+                    //.padding(.vertical, -15)
+            }
+        }.listStyle(.plain)
+        .background(Color.red.ignoresSafeArea())
+        
     }
+    
+    func room(model: RoomModel) -> some View {
+        return ZStack {
+            Rectangle()
+                .foregroundColor(.white)
+                .frame(height: 150)
+                .cornerRadius(20)
+                //.overlay(
+                //    RoundedRectangle(cornerRadius: 20)
+                //        .stroke(Color.black, lineWidth: 5)
+                //)
+            HStack {
+                VStack(alignment: .leading){
+                    Text(model.name)
+                        .font(.system(size: 20, weight: .bold))
+                        .foregroundColor(.black)
+                    
+                }
+                .padding(.leading,20)
+                Spacer()
+                Text("Join")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(.black)
+                    .padding(10)
+                    .background(.red)
+                    .cornerRadius(20)
+                    .padding(.trailing,20)
+                    
+                    
+                
+            }//.frame(height: 150).fixedSize()
+        }
+        .listRowBackground(Color.red)
+        .listRowSeparator(.hidden)
+    }
+    
 }
 
 struct RoomsScreen_Previews: PreviewProvider {
