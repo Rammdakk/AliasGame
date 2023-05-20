a3ee77251df48fbea0fc1e03c0019457ed124b65
Marina <maroschupkina@yandex-team.ru>
2023-05-20 18:24:54 +0300

    merge fix

--- AliasGame/Rooms/GameRoom/GameRoomScreen.swift
+++ AliasGame/Rooms/GameRoom/GameRoomScreen.swift
@@ -66,62 +66,67 @@ struct GameRoomScreen: View {
         VStack(alignment: .leading){
             switch navigationState {
             case .GameRoom(let room):
-            HStack {
+                HStack {
                     Text("Room name: \(room.name)" )
                         .font(.system(size: 30, weight: .heavy))
                         .foregroundColor(.black).padding(.horizontal)
-                Spacer()
-                Button(action: {showSettings.toggle()}){
-                    Image(systemName: "gearshape.fill")
-                        .font(.title)
-                        .foregroundColor(.black)
-                        .padding()
-                }
-                
-            }.padding()
-
-            VStack(alignment:.leading, spacing: 5){
-                Text("ID: 09518E1B-4D96-45D1-8E5F-6E756C8A98A4")
-                    .font(.system(size: 10, weight: .bold))
-                    .foregroundColor(.black)
-                    .padding(.horizontal)
-                
-                Text("Code: CODE")
-                    .font(.system(size: 15, weight: .bold))
-                    .foregroundColor(.black)
-                    .padding(.horizontal)
-                
-                
-            }
-            .padding()
-            .textSelection(.enabled)
-        }.padding(.vertical)
-    }
-    
-    func player(model: PlayerModel) -> some View {
-        return ZStack(alignment: .leading) {
-            Rectangle()
-                .foregroundColor(.white)
-                .frame(height: 50)
-                .cornerRadius(10)
-            VStack {
-                HStack{
-                    Text(model.name)
-                        .font(.system(size: 20, weight: .bold))
-                        .foregroundColor(.black)
                     Spacer()
-                    Text("0")
-                        .font(.system(size: 16, weight: .bold))
-                        .foregroundColor(.black)
-                        .padding()
-                }
+                    Button(action: {showSettings.toggle()}){
+                        Image(systemName: "gearshape.fill")
+                            .font(.title)
+                            .foregroundColor(.black)
+                            .padding()
+                    }
+                    
+                }.padding()
                 
-            }
-            .padding(.leading,20)
+                VStack(alignment:.leading, spacing: 5){
+                    Text("ID: 09518E1B-4D96-45D1-8E5F-6E756C8A98A4")
+                        .font(.system(size: 10, weight: .bold))
+                        .foregroundColor(.black)
+                        .padding(.horizontal)
+                    
+                    Text("Code: CODE")
+                        .font(.system(size: 15, weight: .bold))
+                        .foregroundColor(.black)
+                        .padding(.horizontal)
+                    
+                    
+                }
+                .padding()
+                .textSelection(.enabled)
+            default:
+                Text("Error" )
+                    .font(.system(size: 30, weight: .heavy))
+                    .foregroundColor(.black).padding(.horizontal)
         }
-        .listRowBackground(Color.red)
-        .listRowSeparator(.hidden)
+    }.padding(.vertical)
+}
+
+func player(model: PlayerModel) -> some View {
+    return ZStack(alignment: .leading) {
+        Rectangle()
+            .foregroundColor(.white)
+            .frame(height: 50)
+            .cornerRadius(10)
+        VStack {
+            HStack{
+                Text(model.name)
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(.black)
+                Spacer()
+                Text("0")
+                    .font(.system(size: 16, weight: .bold))
+                    .foregroundColor(.black)
+                    .padding()
+            }
+            
+        }
+        .padding(.leading,20)
     }
+    .listRowBackground(Color.red)
+    .listRowSeparator(.hidden)
+}
 }
 
 struct GameRoomScreen_Previews: PreviewProvider {
